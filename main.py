cf762635c86d4a0a5cf6e25f73585a195171ba2f
PinkFloyd92 <ascacere92@gmail.com>
2016-01-24 11:04:18 -0500

    manejo de excepciones por muerte de proceso SE MUERE EL SUPERVISOR

--- main.py
+++ main.py
@@ -14,12 +14,32 @@ def main():
     # supervisor.printProcess()
     # print(nimbus.memory_info[1])
     # print(nimbus.io_counters)
-    while(1):
+    try:
+        zookeeper.setAll()
+        zookeeper.saveProcess()
+    except:
+        print("Error, demonio de zookeeper está muerto.")
+    try:
+        supervisor.setAll()
+        supervisor.saveProcess()
+    except:
+        print("Demonio del supervisor está muerto.")
+    try:
+        nimbus.setAll()
+        nimbus.saveProcess()
+    except:
+        print("Error, demonio de nimbus está muerto.")
+
+
+    
+"""   while(1):
         zookeeper.setAll()
         supervisor.setAll()
         nimbus.setAll()
         nimbus.saveProcess()
         zookeeper.saveProcess()
         supervisor.saveProcess()
-        time.sleep(.200)
+        # time.sleep(.200)"""
+
+
 main()
